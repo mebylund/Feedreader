4c8d3c5eb119040633c67c9e6c594fc5a47327b9
mebylund <35643665+mebylund@users.noreply.github.com>
2018-09-18 20:51:24 -0700

    fixed extra clone

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -20,7 +20,7 @@ $(function() {
          * the rest of this project. What happens when you change
          * allFeeds in app.js to be an empty array and refresh the
          * page?
-         */
+         */ 
         it('are defined', function() {
             expect(allFeeds).toBeDefined();
             expect(allFeeds.length).not.toBe(0);
@@ -31,28 +31,50 @@ $(function() {
          * in the allFeeds object and ensures it has a URL defined
          * and that the URL is not empty.
          */
+        it('URL defined and not empty', function() {
+            allFeeds.forEach(function(obj) {
+            expect(obj.url).toBeDefined();
+            expect(obj.url).not.toBe('');
+            });
+        });
 
 
         /* TODO: Write a test that loops through each feed
          * in the allFeeds object and ensures it has a name defined
          * and that the name is not empty.
          */
+        it('name defined and not empty', function() {
+            allFeeds.forEach(function(obj){
+            expect(obj.name).toBeDefined();
+            expect(obj.name).not.toBe('');
+            });
+        });
     });
 
 
     /* TODO: Write a new test suite named "The menu" */
-
+    describe('the menu', function() {
+        
         /* TODO: Write a test that ensures the menu element is
          * hidden by default. You'll have to analyze the HTML and
          * the CSS to determine how we're performing the
          * hiding/showing of the menu element.
          */
+        it("menu is hidden by defult", function() {
+            expect
+        });
 
          /* TODO: Write a test that ensures the menu changes
           * visibility when the menu icon is clicked. This test
           * should have two expectations: does the menu display when
           * clicked and does it hide when clicked again.
           */
+         it('menu changes to correct icon', function(){
+             
+         });
+
+    });
+
 
     /* TODO: Write a new test suite named "Initial Entries" */
 
